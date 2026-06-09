d85bb466c64605ffb5f29eba930bf9659b8c8010
aidan102004 <aidan.abidi@gmail.com>
2026-06-09 23:52:21 +1000

    removed rudendent code

--- src/main.cpp
+++ src/main.cpp
@@ -32,10 +32,6 @@ GLuint indices[] =
 
 const int width = 800, height = 800;
 
-unsigned char pixels[width * height * 4];
-unsigned char cloudpixels[width * height * 4];
-unsigned char moonpixels[width * height * 4];
-
 int gridSize = 180;
 void drawCircle(VAO& vao, Shader& shaderProgram, GLuint ldUniform, glm::vec3 ldr, GLuint rot, GLuint lrot, glm::mat3 rotMtx, glm::mat3 lightRot, float alpha, glm::mat4 proj, glm::mat4 view, glm::mat4 model, glm::vec3 scale, glm::vec3 transform, Pallete& col, float z, int gridSize, int octCount, int seed);
 int main()
@@ -67,19 +63,6 @@ int main()
 
 	VBO VBO1(vertices, sizeof(vertices));
 	EBO EBO1(indices, sizeof(indices));
-	//circle 2s vao, vbo, ebo
-	VAO VAO2;
-	VAO2.Bind();
-	VBO VBO2(vertices, sizeof(vertices));
-	EBO EBO2(indices, sizeof(indices));
-	VAO2.Unbind();
-
-	VAO VAO3;
-	VAO3.Bind();
-	VBO VBO3(vertices, sizeof(vertices));
-	EBO EBO3(indices, sizeof(indices));
-	VAO3.Unbind();
-
 	//circle 1
 	VAO1.Bind();
 	VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 5 * sizeof(float), (void*)0);
@@ -88,61 +71,15 @@ int main()
 	VBO1.Unbind();
 	EBO1.Unbind();
 
-	VAO2.Bind();
-	VAO2.LinkAttrib(VBO2, 0, 3, GL_FLOAT, 5 * sizeof(float), (void*)0);
-    VAO2.LinkAttrib(VBO2, 1, 2, GL_FLOAT, 5 * sizeof(float), (void*)(3 * sizeof(float)));
-	VAO2.Unbind();
-	VBO2.Unbind();
-	EBO2.Unbind();
-
-	VAO3.Bind();
-	VAO3.LinkAttrib(VBO3, 0, 3, GL_FLOAT, 5 * sizeof(float), (void*)0);
-    VAO3.LinkAttrib(VBO3, 1, 2, GL_FLOAT, 5 * sizeof(float), (void*)(3 * sizeof(float)));
-	VAO3.Unbind();
-	VBO3.Unbind();
-	EBO3.Unbind();
-
 	int seed = 0, cloudSeed = 0, moonSeed = 0;
 	srand(time(NULL));
 	seed = rand();
 	cloudSeed = rand();
 	moonSeed = rand();
 
-	Pallete earthPallete = {glm::vec4(15, 70, 160, 255), glm::vec4(24, 107, 202, 255), glm::vec4(194, 178, 128, 255), glm::vec4(44, 112, 49, 255), glm::vec4(0.0, 0.1, 0.14, 1.0) };
-	Perlin perlin(pixels, 6, seed, gridSize, {width, height}, earthPallete);
-	GLuint perlinTex;
-	glGenTextures(1, &perlinTex);
-	glBindTexture(GL_TEXTURE_2D, perlinTex);
-
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0,
-             GL_RGBA, GL_UNSIGNED_BYTE, pixels);
-
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-
+	Pallete earthPallete = {glm::vec4(15, 70, 160, 255), glm::vec4(24, 107, 202, 255), glm::vec4(194, 178, 128, 255), glm::vec4(44, 112, 49, 255), glm::vec4(0.0, 0.1, 0.14, 1.0)};
 	Pallete cloudPallete = {glm::vec4(237, 237, 237, 237),glm::vec4(34, 139, 34, 0), glm::vec4(34, 139, 34, 0), glm::vec4(34, 139, 34, 0), glm::vec4(0.0, 0.1, 0.14, 1.0)};
-	Perlin cloudPerlin(cloudpixels, 4, cloudSeed, 80, {width, height}, cloudPallete);
-	GLuint cloudTex;
-	glGenTextures(1, &cloudTex);
-	glBindTexture(GL_TEXTURE_2D, cloudTex);
-
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0,
-             GL_RGBA, GL_UNSIGNED_BYTE, cloudpixels);
-
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-
 	Pallete moonPallete = {glm::vec4(170, 170, 165, 255), glm::vec4(110, 110, 115, 255), glm::vec4(60, 60, 65, 255), glm::vec4(220, 215, 200, 255), glm::vec4(0.2, 0.7, 0.8, 1.0)};
-	Perlin moonPerlin(moonpixels, 4, moonSeed, 80, {width, height}, moonPallete);
-	GLuint moonTex;
-	glGenTextures(1, &moonTex);
-	glBindTexture(GL_TEXTURE_2D, moonTex);
-
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0,
-             GL_RGBA, GL_UNSIGNED_BYTE, moonpixels);
-
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
 	glm::vec3 lightur = glm::normalize(glm::vec3(1.0,1.0,1.0));
 	GLuint ldUniform = glGetUniformLocation(shaderProgram.ID, "uLightDir");
@@ -191,7 +128,7 @@ int main()
 			-sin(angle2 + offset), 0, cos(angle2 + offset));
 		drawCircle(VAO1, shaderProgram, ldUniform, lightur, rot_uniform, light_uniform, rotMatrix, rotMatrix, 1.0f, proj, view, glm::mat4(1.0f), glm::vec3(1.0f), glm::vec3(0.0f), earthPallete, 1.0, gridSize, 6, seed);
 
-		drawCircle(VAO2, shaderProgram, ldUniform, lightur, rot_uniform, light_uniform, rotMatrix2, rotMatrix, 0.8f, proj, view, glm::mat4(1.0f), glm::vec3(1.2f), glm::vec3(0.0f), cloudPallete, (float)glfwGetTime() * 0.3, 80, 4, cloudSeed);
+		drawCircle(VAO1, shaderProgram, ldUniform, lightur, rot_uniform, light_uniform, rotMatrix2, rotMatrix, 0.8f, proj, view, glm::mat4(1.0f), glm::vec3(1.2f), glm::vec3(0.0f), cloudPallete, (float)glfwGetTime() * 0.3, 80, 4, cloudSeed);
 
 		//drawCircle(VAO3, shaderProgram, ldUniform, lightur, rot_uniform, light_uniform, rotMatrix2, rotMatrix, 1.0f, proj, view, glm::mat4(1.0f), glm::vec3(0.2f), glm::vec3(5.0f, 1.0f, 0.5f), moonPallete, 1.0, 80, 4, moonSeed);
 
