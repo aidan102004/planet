b71dd959766a82292f5c1e9f89b392acd02aa89d
aidan102004 <aidan.abidi@gmail.com>
2026-06-10 17:29:40 +1000

    added proper rotating light

--- src/main.cpp
+++ src/main.cpp
@@ -13,6 +13,7 @@
 #include"ebo.h"
 #include"perlin.h"
 #include"pallette.h"
+#include"planet.h"
 
 
 GLfloat vertices[] =
@@ -78,7 +79,7 @@ int main()
 	moonSeed = rand();
 
 	Pallete earthPallete = {glm::vec4(15, 70, 160, 255), glm::vec4(24, 107, 202, 255), glm::vec4(194, 178, 128, 255), glm::vec4(44, 112, 49, 255), glm::vec4(0.0, 0.1, 0.14, 1.0)};
-	Pallete cloudPallete = {glm::vec4(237, 237, 237, 237),glm::vec4(34, 139, 34, 0), glm::vec4(34, 139, 34, 0), glm::vec4(34, 139, 34, 0), glm::vec4(0.0, 0.1, 0.14, 1.0)};
+	Pallete cloudPallete = {glm::vec4(237, 237, 237, 237),glm::vec4(34, 139, 34, 0), glm::vec4(34, 139, 34, 0), glm::vec4(34, 139, 34, 0), glm::vec4(-0.1, 0.1, 0.14, 1.0)};
 	Pallete moonPallete = {glm::vec4(170, 170, 165, 255), glm::vec4(110, 110, 115, 255), glm::vec4(60, 60, 65, 255), glm::vec4(220, 215, 200, 255), glm::vec4(0.2, 0.7, 0.8, 1.0)};
 
 	glm::vec3 lightur = glm::normalize(glm::vec3(1.0,1.0,1.0));
@@ -108,7 +109,10 @@ int main()
 	{
 		glClearColor((float)6/255, (float)5/255, (float)33/255, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
-
+		if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS) {
+			randomizePlanet();
+		}
+        
 		shaderProgram.Activate();
 		
 		double currTime = glfwGetTime();
@@ -126,11 +130,16 @@ int main()
 			cos(angle2 + offset), 0, sin(angle2 + offset),
 			0, 1, 0,
 			-sin(angle2 + offset), 0, cos(angle2 + offset));
-		drawCircle(VAO1, shaderProgram, ldUniform, lightur, rot_uniform, light_uniform, rotMatrix, rotMatrix, 1.0f, proj, view, glm::mat4(1.0f), glm::vec3(1.0f), glm::vec3(0.0f), earthPallete, 1.0, gridSize, 6, seed);
 
-		drawCircle(VAO1, shaderProgram, ldUniform, lightur, rot_uniform, light_uniform, rotMatrix2, rotMatrix, 0.8f, proj, view, glm::mat4(1.0f), glm::vec3(1.2f), glm::vec3(0.0f), cloudPallete, (float)glfwGetTime() * 0.3, 80, 4, cloudSeed);
+		glm::mat3 lightMatrix = glm::mat3(
+			cos(-angle * 0.4f), 0, sin(-angle* 0.4f),
+			0, 1, 0,
+			-sin(-angle* 0.4f), 0, cos(-angle* 0.4f));
+		drawCircle(VAO1, shaderProgram, ldUniform, lightur, rot_uniform, light_uniform, rotMatrix, lightMatrix, 1.0f, proj, view, glm::mat4(1.0f), glm::vec3(1.0f), glm::vec3(0.0f), earthPallete, 1.0, gridSize, 6, seed);
 
-		//drawCircle(VAO3, shaderProgram, ldUniform, lightur, rot_uniform, light_uniform, rotMatrix2, rotMatrix, 1.0f, proj, view, glm::mat4(1.0f), glm::vec3(0.2f), glm::vec3(5.0f, 1.0f, 0.5f), moonPallete, 1.0, 80, 4, moonSeed);
+		drawCircle(VAO1, shaderProgram, ldUniform, lightur, rot_uniform, light_uniform, rotMatrix2, lightMatrix, 0.8f, proj, view, glm::mat4(1.0f), glm::vec3(1.2f), glm::vec3(0.0f), cloudPallete, (float)glfwGetTime() * 0.3, 80, 4, cloudSeed);
+
+		//drawCircle(VAO1, shaderProgram, ldUniform, lightur, rot_uniform, light_uniform, rotMatrix2, rotMatrix, 1.0f, proj, view, glm::mat4(1.0f), glm::vec3(0.2f), glm::vec3(4.5f, 1.5f, 1.5f), moonPallete, 1.0, 80, 4, moonSeed);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
@@ -145,7 +154,7 @@ int main()
 	return 0;
 }
 
-void drawCircle(VAO& vao, Shader& shaderProgram, GLuint ldUniform, glm::vec3 ldr, GLuint rot, GLuint lrot, glm::mat3 rotMtx, glm::mat3 lightRot, float alpha, glm::mat4 proj, glm::mat4 view, glm::mat4 model, glm::vec3 scale, glm::vec3 transform, Pallete& col, float z, int gridSize, int octCount, int seed) {
+void drawCircle(VAO& vao, Shader& shaderProgram, GLuint ldUniform, glm::vec3 ldr, GLuint rot, GLuint lrot, glm::mat3 rotMtx, glm::mat3 lightRot, glm::mat4 proj, glm::mat4 view, glm::mat4 model, glm::vec3 scale, glm::vec3 transform, float alpha, Pallete& col, float z, int gridSize, int octCount, int seed) {
 
 		glUniform1f(glGetUniformLocation(shaderProgram.ID, "zComp"), z);
 		glUniform1i(glGetUniformLocation(shaderProgram.ID, "seed"), seed);
@@ -166,4 +175,8 @@ void drawCircle(VAO& vao, Shader& shaderProgram, GLuint ldUniform, glm::vec3 ldr
 		glUniformMatrix3fv(lrot, 1, GL_FALSE, glm::value_ptr(lightRot));
 		vao.Bind();
 		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);	
+}
+
+std::pair<Planet, Planet> randomizePlanet() {
+	
 }
